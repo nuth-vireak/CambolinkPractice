7a8d1ff91a2181b73a8e90860f9ca8a3586c5cfa
nuth-vireak <timberr.4t@gmail.com>
2024-08-23 16:24:12 +0700

    Initial Commit

---
+++ CambolinkPractice/CambolinkPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CambolinkPracticeApp.swift
+//  CambolinkPractice
+//
+//  Created by KAK-REAK on 23/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CambolinkPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CambolinkPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CambolinkPractice
+//
+//  Created by KAK-REAK on 23/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
